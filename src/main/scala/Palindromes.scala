be6dadd52116cfc0d271ce3019b5046bf22fc329
Michal <michal.zeglarski@gmail.com>
2017-05-13 16:55:36 +0200

    Initial text preparation

--- src/main/scala/Palindromes.scala
+++ src/main/scala/Palindromes.scala
@@ -2,17 +2,17 @@
 object Palindromes {
 
   def main(args: Array[String]): Unit = {
-    val textToCheck = "abbaab"
+    val textToCheck = "aba"
     val preparedText = prepare(textToCheck)
     val p = new Array[Int](preparedText.length)
     var center = 0
     var right = 0
-    for (i <- 1 until preparedText.length) {
+    for (i <- 1 until preparedText.length - 1) {
       val mirror = 2 * center - i
       if (i < right)
         p(i) = math.min(right - i, p(mirror))
 
-      while (preparedText.charAt(i + (1 + p(i))) == preparedText.charAt(i - (1 + p(i))))
+      while (preparedText(i + (1 + p(i))) == preparedText(i - (1 + p(i))))
         p(i) += 1
 
       if (i + p(i) > right) {
@@ -26,7 +26,16 @@ object Palindromes {
   }
 
   def prepare(textToCheck: String) : Array[Char] = {
-    new Array[Char](10)
+    val preparedArray = new Array[Char](2 * textToCheck.length + 3)
+    preparedArray(0) = '$'
+    preparedArray(2 * textToCheck.length + 2) = '@'
+    for (i <- 0 until textToCheck.length) {
+      preparedArray(2 * i + 1) = '#'
+      preparedArray(2 * i + 2) = textToCheck.charAt(i)
+    }
+    preparedArray(2 * textToCheck.length + 1) = '#'
+
+    preparedArray
   }
 
 }
