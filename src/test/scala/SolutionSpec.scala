6dfbb72ccf3176598641079b20a804b625bf08b1
Michal <michal.zeglarski@gmail.com>
2017-05-15 15:14:01 +0200

    Test cleanup

--- src/test/scala/SolutionSpec.scala
+++ src/test/scala/SolutionSpec.scala
@@ -1,63 +1,64 @@
 import org.scalatest._
 
 class SolutionSpec extends FlatSpec with Matchers {
-  "Solution" should "return no palindromes in an empty string" in {
+  "Solution" should "calculate no palindromes in an empty string" in {
     val empty = ""
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (0)
   }
 
-  it should "return no palindromes in a single char string" in {
+  it should "calculate no palindromes in a single char string" in {
     val empty = "a"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (0)
   }
 
-  it should "return one palindrome in a double char string" in {
+  it should "calculate one palindrome in a double char string" in {
     val empty = "aa"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (1)
   }
 
-  it should "return one palindrome in a odd number of chars string" in {
+  it should "calculate one palindrome in a odd number of chars string" in {
     val empty = "aba"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (1)
   }
 
-  it should "return two palindromes in found chars string" in {
+  it should "calculate two palindromes in found chars string" in {
     val empty = "abba"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (2)
   }
 
-  it should "return correct number of palindromes for a longer string" in {
+  it should "calculate correct number of palindromes for a longer string" in {
     val empty = "abaabc"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (3)
   }
 
-  it should "return correct number of palindromes for a more complicated string" in {
+  it should "calculate correct number of palindromes for a more complicated string" in {
     val empty = "abaabcbaaab"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (10)
   }
 
-  it should "return correct number of palindromes for a string based on already tested string" in {
+  it should "calculate correct number of palindromes for a string based on already tested string" in {
     val empty = "abaabcbaaabaa"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (13)
   }
 
-  it should "return correct number of palindromes for an even more complicated string" in {
+  it should "calculate correct number of palindromes for an even more complicated string" in {
     val empty = "abaabcbaaabdd"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (11)
   }
 
-  it should "return correct number of palindromes for a string that contains already tested string" in {
+  it should "calculate correct number of palindromes for a string that contains already tested string" in {
     val empty = "abaabcbaaabddb"
     val palindromesCount = Solution.solution(empty)
     palindromesCount should be (12)
   }
+
 }
